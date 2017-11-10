a292fc7d8ecb28c8f35024187c5efc9ba9675c47
brwitten <bethrwitten@gmail.com>
2017-11-10 14:29:54 -0800

    fixed read only issue for theme

--- wheel_of_fortune.rb
+++ wheel_of_fortune.rb
@@ -1,29 +1,36 @@
 class WheelOfFortune
-  attr_accessor :phrase, :remaining_letters, :guesses
+  attr_accessor :phrase, :guesses
 
   def initialize(inputs)
-    @theme = inputs[:theme.freeze] || ""
+    @theme = inputs[:theme] || ""
     @phrase = inputs[:phrase] || ""
     @remaining_letters = []
     @guesses = []
-    p "Happy coding!"
   end
 
+# displays blocks
   def to_s
-    phrase
+    @phrase
+  end
+
+  def theme
+    @theme
   end
 
   def can_i_have?(input)
     guess = input.downcase
+    @guesses << guess
   end
 
   def game_over?
-    if remaining_letters.length > 0 && guesses.length > 0
+    if remaining_letters.length = 0 && guesses.length > 0
       true
     else
       false
     end
   end
+
+
 end
 
 
@@ -36,6 +43,9 @@ if __FILE__ == $0
   j.can_i_have?("g")
   j.can_i_have?("o")
   p j
+  puts j.phrase
+  puts j.theme
+
   puts j # calls j.to_s
 
 end
