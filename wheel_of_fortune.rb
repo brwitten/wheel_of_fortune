9648fb6d76db5867196f04befaf7897b5e260007
brwitten <bethrwitten@gmail.com>
2017-11-12 13:48:29 -0800

    remaining letters updates based on guess result

--- wheel_of_fortune.rb
+++ wheel_of_fortune.rb
@@ -4,29 +4,40 @@ class WheelOfFortune
   def initialize(inputs)
     @theme = inputs[:theme] || ""
     @phrase = inputs[:phrase] || ""
-    @remaining_letters = []
+    @remaining_letters = clean_phrase
     @guesses = []
   end
 
-# displays blocks
+  #displays blocks
   def to_s
     @phrase
+    @phrase.gsub(remaining_letters, "_")
   end
 
+  #handles guess
   def can_i_have?(input)
     guess = input.downcase
     lower_phrase = @phrase.downcase
-    @guesses << guess
-    puts "CLEANED PHRASE: #{clean_phrase}"
-    if clean_phrase.include? (guess)
-      puts "Great guess!"
+    if guesses.include? (guess)
+      puts "you already guessed this!"
     else
-      puts "Try again.."
+      @guesses << guess
+      puts "CLEAN PHRASE: #{clean_phrase}"
+      if clean_phrase.include? (guess)
+        puts "Great guess!"
+        @remaining_letters.delete(guess)
+        puts @remaining_letters
+        true
+      else
+        puts "Try again.."
+        false
+      end
     end
   end
 
+  #is the game over?
   def game_over?
-    if remaining_letters.length = 0 && guesses.length > 0
+    if @remaining_letters.length == 0 && guesses.length > 0
       true
     else
       false
@@ -38,7 +49,7 @@ class WheelOfFortune
     @theme
   end
 
-  # cleaning the phrase to distinct letters, no spaces
+  # cleaning the phrase to distinct letters;
   def clean_phrase
     clean_phrase = @phrase.to_s
     clean_phrase.downcase!
@@ -50,6 +61,7 @@ class WheelOfFortune
 end
 
 
+
 # Checks to see if we executed this file from the command-line
 # e.g. `ruby simple_jepordy.rb`
 if __FILE__ == $0
@@ -57,10 +69,8 @@ if __FILE__ == $0
   # DRIVER CODE
   j = WheelOfFortune.new( {theme: "card games", phrase: "Go fish"} )
   j.can_i_have?("g")
+  j.can_i_have?("g")
   j.can_i_have?("o")
   p j
-  # puts j.guess
-
-  puts j # calls j.to_s
 
 end
